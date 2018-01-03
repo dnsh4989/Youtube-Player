f7efa3da3dbbacb443fd24a08fe1884802e16c24
Dinesh Sanala <dnsh4989@gmail.com>
2018-01-03 21:11:10 +0530

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { YoutubePlaylistService, Playlist, HttpUtilService} from 'angular2-youtube';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private playlist:Playlist[];
+  private playlistYoutube:any;
+  private msgErro: string;
+
+  // player created and initialized - sends instance of the player
+  @Output() ready = new EventEmitter<YT.Player>();
+  // state change: send the YT event with its state
+  @Output() change = new EventEmitter<YT.PlayerEvent>();
+
+  constructor(private youtubePlaylist:YoutubePlaylistService) { }
+  
   title = 'My Video Player';
+  videos;
+  playListId;
+  player: YT.Player;
+  fetched = false;
+  id: string = '';
+  counter=0;
+  private key: string = "AIzaSyAsD7pMG9GkWGp9G68wsJvZwXz50wie99s";
+
+  savePlayer (player) {
+    this.player = player;
+    console.log('player instance', player)
+  }
+
+  onStateChange(event){
+    console.log('player state', event);
+  }
+
+  getUniqueID(url){
+  	return url.split("list=")[1];
+  }
+
+  fetch(event, url){
+  	console.log(event);
+  	console.log(url.value);
+  	this.playListId = this.getUniqueID(url.value);
+  	console.log(this.playListId);
+  	this.getplaylist();
+  }
+
+  getplaylist() {
+    this.playlistYoutube = this.youtubePlaylist.getplaylist(this.key, this.playListId)
+    .subscribe(value => {
+		this.playlistYoutube = value;
+		console.log(this.playlistYoutube);
+		console.log(this.playlistYoutube.items);
+		this.id = this.playlistYoutube.items[0].snippet.resourceId.videoId;
+		this.videos = this.playlistYoutube.items;
+		console.log(this.id);
+		this.player.loadVideoById(this.id);
+		this.fetched = true;
+    });
+    console.log('s');
+  }
+
+  nextPage(pageToken:string){
+    this.playlistYoutube = this.youtubePlaylist.playlistList_page(this.key, this.playListId, pageToken).subscribe(value => {
+              this.playlistYoutube = value;
+               console.log(this.playlistYoutube);
+               console.log(this.playlistYoutube.items);
+               this.counter++;
+    });
+  }
+
+  prevPage(pageToken:string){
+    this.playlistYoutube = this.youtubePlaylist.playlistList_page(this.key, this.playListId, pageToken).subscribe(value => {
+              this.playlistYoutube = value;
+               console.log(this.playlistYoutube);
+               console.log(this.playlistYoutube.items);
+               this.counter--;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { YoutubePlayerModule } from 'ng2-youtube-player';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { YoutubePlaylistService,HttpUtilService } from 'angular2-youtube';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YoutubePlayerModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [YoutubePlaylistService,HttpUtilService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
